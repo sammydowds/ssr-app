e3e57fac095f1ef9d6dd50012cc7a0d9bb82e31e
sammydowds <sammydowds1993@gmail.com>
2021-08-15 12:56:42 -0500

    feat: client side stuff

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { HomeHeader } from './Home'
+
+function App() {
+  return (
+    <HomeHeader name="sammy" />
+  );
+}
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,7 @@
+
+import React from 'react'
+
+export const HomeHeader = props => {
+  return <h1>Hello {props.name}!</h1>;
+};
+
